9f953e97240884898a4a0d2a6fdb9e892fc6247c
Chris East <chris.east95@gmail.com>
2023-11-11 17:19:06 -0500

    Updates

--- 2 - Decorator/src/PureJS/coffee.js
+++ 2 - Decorator/src/PureJS/coffee.js
@@ -1,41 +1,41 @@
 class Size {
-    static TALL = 'TALL';
-    static GRANDE = 'GRANDE';
-    static VENTI = 'VENTI';
+  static TALL = "TALL";
+  static GRANDE = "GRANDE";
+  static VENTI = "VENTI";
 }
 
 class Beverage {
-    constructor() {
-        this.size = Size.TALL;
-        this.description = "Unknow Beverage"
-    }
-    getDescription() {
-        return this.description;
-    }
+  constructor() {
+    this.size = Size.TALL;
+    this.description = "Unknow Beverage";
+  }
+  getDescription() {
+    return this.description;
+  }
 
-    setSize(size) {
-        this.size = size
-    }
+  setSize(size) {
+    this.size = size;
+  }
 
-    getSize() {
-        return this.size
-    }
+  getSize() {
+    return this.size;
+  }
 }
 
 class CondimentDecorator extends Beverage {
-    constructor(beverage) {
-      super();
-      this.beverage = beverage;
-    }
-  
-    getSize() {
-        return this.beverage.getSize();
-    }
+  constructor(beverage) {
+    super();
+    this.beverage = beverage;
+  }
 
-    getDescription() {
-      // This method needs to be implemented in concrete subclasses.
-      throw new Error('getDescription must be implemented in subclasses');
-    }
+  getSize() {
+    return this.beverage.getSize();
+  }
+
+  getDescription() {
+    // This method needs to be implemented in concrete subclasses.
+    throw new Error("getDescription must be implemented in subclasses");
+  }
 }
 
 class Espresso extends Beverage {
@@ -50,97 +50,96 @@ class Espresso extends Beverage {
 }
 
 class HouseBlend extends Beverage {
-    constructor() {
-        super();
-        this.description = "House Blend Coffee";
-    }
+  constructor() {
+    super();
+    this.description = "House Blend Coffee";
+  }
 
-    cost() {
-        return 0.89
-    }
+  cost() {
+    return 0.89;
+  }
 }
 
 class DarkRoast extends Beverage {
-    constructor() {
-        super();
-        this.description = "Dark Roast Coffee";
-    }
+  constructor() {
+    super();
+    this.description = "Dark Roast Coffee";
+  }
 
-    cost() {
-        return 0.99
-    }
+  cost() {
+    return 0.99;
+  }
 }
 
 class Mocha extends CondimentDecorator {
-    constructor(beverage) {
-        super();
-        this.beverage = beverage
-    }
+  constructor(beverage) {
+    super();
+    this.beverage = beverage;
+  }
 
-    getDescription() {
-        return this.beverage.getDescription() + ", Mocha";
-    }
+  getDescription() {
+    return this.beverage.getDescription() + ", Mocha";
+  }
 
-    cost() {
-        return this.beverage.cost() + .20;
-    }
+  cost() {
+    return this.beverage.cost() + 0.2;
+  }
 }
 
 class Whip extends CondimentDecorator {
-    constructor(beverage) {
-        super();
-        this.beverage = beverage
-    }
+  constructor(beverage) {
+    super();
+    this.beverage = beverage;
+  }
 
-    getDescription() {
-        return this.beverage.getDescription() + ", Whip";
-    }
+  getDescription() {
+    return this.beverage.getDescription() + ", Whip";
+  }
 
-    cost() {
-        var cost = this.beverage.cost()
-        console.log(this.getSize());
-        if (this.beverage.getSize() == Size.TALL) {
-            cost += .10;
-         
-        } else if (this.beverage.getSize() == Size.GRANDE) {
-            cost += .15;
-        } else if (this.beverage.getSize() == Size.VENTI) {
-            cost += .20;
-        } else {
-            console.log("Else");
-        }
-        return cost + .10;
+  cost() {
+    var cost = this.beverage.cost();
+    console.log(this.getSize());
+    if (this.beverage.getSize() == Size.TALL) {
+      cost += 0.1;
+    } else if (this.beverage.getSize() == Size.GRANDE) {
+      cost += 0.15;
+    } else if (this.beverage.getSize() == Size.VENTI) {
+      cost += 0.2;
+    } else {
+      console.log("Else");
     }
+    return cost + 0.1;
+  }
 }
 
 class Soy extends CondimentDecorator {
-    constructor(beverage) {
-        super();
-        this.beverage = beverage
-    }
+  constructor(beverage) {
+    super();
+    this.beverage = beverage;
+  }
 
-    getDescription() {
-        return this.beverage.getDescription() + ", Soy";
-    }
+  getDescription() {
+    return this.beverage.getDescription() + ", Soy";
+  }
 
-    cost() {
-        return this.beverage.cost() + .15;
-    }
+  cost() {
+    return this.beverage.cost() + 0.15;
+  }
 }
 
 class StarbuzzCoffee {
-    constructor() {
-        this.beverage = new Espresso();
-        console.log(this.beverage.getDescription() + " $" + this.beverage.cost())
-        
-        this.beverage2 = new DarkRoast();
-        this.beverage2.setSize(Size.VENTI);
-        console.log(this.beverage2.getSize())
-        this.beverage2 = new Soy(this.beverage2);
-        this.beverage2 = new Mocha(this.beverage2);
-        this.beverage2 = new Whip(this.beverage2);
-        console.log(this.beverage2.getDescription() + " $" + this.beverage2.cost())
-    }
+  constructor() {
+    this.beverage = new Espresso();
+    console.log(this.beverage.getDescription() + " $" + this.beverage.cost());
+
+    this.beverage2 = new DarkRoast();
+    this.beverage2.setSize(Size.VENTI);
+    console.log(this.beverage2.getSize());
+    this.beverage2 = new Soy(this.beverage2);
+    this.beverage2 = new Mocha(this.beverage2);
+    this.beverage2 = new Whip(this.beverage2);
+    console.log(this.beverage2.getDescription() + " $" + this.beverage2.cost());
+  }
 }
 
 const starbuzzCoffee = new StarbuzzCoffee();
--- 3 - Factory/src/mine/observer.js
+++ 3 - Factory/src/mine/observer.js
@@ -1,28 +1,28 @@
 class Observer {
-    constructor() {
-        this.observers = [];
-    }
+  constructor() {
+    this.observers = [];
+  }
 
-    registerObserver(observer) {
-        this.observers.push(observer);
-    }
+  registerObserver(observer) {
+    this.observers.push(observer);
+  }
 
-    removeObserver(observer) {
-        const index = this.observers.indexOf(observer);
-        if (index !== -1){
-            this.observers.splice(index,1)
-        }
+  removeObserver(observer) {
+    const index = this.observers.indexOf(observer);
+    if (index !== -1) {
+      this.observers.splice(index, 1);
     }
+  }
 
-    notifyObservsers() {
-        for (const observer of this.observers) {
-            observer.reveal();
-        }
+  notifyObservsers() {
+    for (const observer of this.observers) {
+      observer.reveal();
     }
+  }
 
-    bombTriggered() {
-        this.notifyObservsers();
-    }
+  bombTriggered() {
+    this.notifyObservsers();
+  }
 }
 
 export default Observer;
